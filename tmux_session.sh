433da023f6efe2be4cf19161a540c2e9da7c3cb1
dhuck <dhuck@tinycact.us>
2023-01-29 13:00:35 -0600

    generalize tmux to work with install path param

--- tmux_session.sh
+++ tmux_session.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
+tmux -V > /dev/null 2>&1
+if [ $? -eq 127 ]
+then
+    echo "tmux is not installed! Cannot continue"
+    exit 127
+fi
+
 session="ROS"
+
+INSTALL_PATH=$1
+if [ $INSTALL_PATH -eq ""]
+then
+    INSTALL_PATH="${HOME}/"
+fi
 
 tmux new-session -d -s $session 
 tmux set mouse on
@@ -11,10 +24,10 @@ tmux send-keys -t $session:$window 'roscore' C-m
 
 window=1
 tmux new-window -t $session:$window -n 'simulator'
-tmux send-keys -t $session:$window 'cd ~/ut_automata && ./bin/simulator --localize' C-m
+tmux send-keys -t $session:$window "cd ${INSTALL_PATH}/ut_automata && ./bin/simulator --localize" C-m
 
 window=2
 tmux new-window -t $session:$window -n 'websocket'
-tmux send-keys -t $session:$window 'cd ~/ut_automata && ./bin/websocket' C-m
+tmux send-keys -t $session:$window "cd ${INSTALL_PATH}/ut_automata && ./bin/websocket" C-m
 
 /bin/bash
